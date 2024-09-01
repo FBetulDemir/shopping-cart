17f263ade0a3fa4d12c23e42adabd60353743a58
FBetulDemir <fatmabetuld@gmail.com>
2024-09-01 10:17:45 +0200

    Create Products list and functions to add calculate and display Shopping cart

--- shoppingCart.js
+++ shoppingCart.js
@@ -5,3 +5,16 @@ var Categories;
     Categories["GROCERIES"] = "Groceries";
     Categories["CLOTHING"] = "Clothing";
 })(Categories || (Categories = {}));
+const products = [
+    { id: 1, name: "Laptop", price: 999.99, category: Categories.ELECTRONICS },
+    { id: 2, name: "T-Shirt", price: 19.99, category: Categories.CLOTHING },
+    { id: 3, name: "Bananas", price: 1.99, category: Categories.GROCERIES },
+];
+const shoppingCart = [];
+const addToCart = (product) => { };
+const calculateTotal = (cart) => {
+    let total = 0;
+    let message = `Total amount to be paid is ${total}`;
+    return message;
+};
+const displayCart = (cart) => { };
--- shoppingCart.ts
+++ shoppingCart.ts
@@ -6,6 +6,23 @@ enum Categories {
 
 interface Product {
   id: number;
+  name: string;
   price: number;
   category: Categories;
 }
+
+const products: Product[] = [
+  { id: 1, name: "Laptop", price: 999.99, category: Categories.ELECTRONICS },
+  { id: 2, name: "T-Shirt", price: 19.99, category: Categories.CLOTHING },
+  { id: 3, name: "Bananas", price: 1.99, category: Categories.GROCERIES },
+];
+
+const shoppingCart: Product[] = [];
+
+const addToCart = (product: Product): void => {};
+const calculateTotal = (cart: Product[]): string => {
+  let total = 0;
+  let message = `Total amount to be paid is ${total}`;
+  return message;
+};
+const displayCart = (cart: Product[]): void => {};
